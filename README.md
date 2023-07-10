b84e15efe745a65e4695d83a4c258dc2e3a137b1
Samuel Gunter <samathingamajig@gmail.com>
2023-07-09 20:27:55 -0500

    inital commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# obsidian-slides-to-png
+
+To install dependencies:
+
+```bash
+npm install
+```
+
+To run:
+
+```bash
+npx tsx index.ts
+```
---
+++ index.ts
@@ -0,0 +1,75 @@
+import { chromium, devices } from "playwright";
+import { Command } from "commander";
+import { z } from "zod";
+import path from "node:path";
+
+const program = new Command();
+
+program
+  .option("-w, --width <width>", "Width of the output", Number, 1280)
+  .option("-h, --height <height>", "Height of the output, -1 defaults to 16 by 9 ratio of width", Number, -1)
+  .option("-o, --output-dir <output-dir>", "Output directory", String, "slides")
+  .argument("[url]", "URL to process", String, "http://localhost:3000")
+  .parse(process.argv);
+
+const argsSchema = z
+  .object({
+    url: z.string().url(),
+    width: z.number().int().positive(),
+    height: z.number().int(),
+    outputDir: z.string(),
+  })
+  .transform((data, ctx) => {
+    if (data.height === -1) {
+      data.height = Math.round((9 / 16) * data.width);
+    } else if (data.height <= 0) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: "Height must be positive if specified",
+        path: ["height"],
+      });
+      return z.NEVER;
+    }
+
+    return data;
+  });
+
+const safeArgs = argsSchema.safeParse({
+  ...program.opts(),
+  url: program.processedArgs[0],
+});
+
+if (!safeArgs.success) {
+  console.error("Invalid arguments:", safeArgs.error.issues);
+  process.exit(1);
+}
+
+const args = safeArgs.data;
+
+console.log(args);
+
+const browser = await chromium.launch();
+const context = await browser.newContext({
+  ...devices["Desktop Chrome"],
+  viewport: {
+    width: args.width,
+    height: args.height,
+  },
+});
+const page = await context.newPage();
+await page.goto(args.url);
+
+await page.waitForSelector(".drop:not(.slide-background)");
+
+const totalSlides = (await page.$$(".drop:not(.slide-background)")).length;
+const totalSlidesCharLength = String(totalSlides).length;
+
+for (let slide = 1; slide <= totalSlides; slide++) {
+  await page.screenshot({
+    path: path.join(args.outputDir, `slide-${String(slide).padStart(totalSlidesCharLength, "0")}.png`),
+  });
+  await page.keyboard.press("ArrowRight");
+}
+
+await page.close();
+await browser.close();
